29027ecc8d61c011d05f8f3ea41d5747ca041a17
Sophia Rosset <rossetsophia@gmail.com>
2020-06-01 10:45:05 -0400

    Add bio and LinkedIN and GitHub links

---
+++ portfolio/target/portfolio-1/script.js
@@ -0,0 +1,30 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Adds a random fact about me to the page.
+ */
+function addRandomFact() {
+  const facts =
+      ['I am half Brazilian', 'I love long distance running', 'I play the piano'];
+
+  // Pick a random greeting.
+  const fact = facts[Math.floor(Math.random() * facts.length)];
+
+  // Add it to the page.
+  const factContainer = document.getElementById('fact-container');
+  factContainer.innerText = fact;
+  
+  console.log("Testing JavaScript console!");
+}
